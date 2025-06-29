a1c6c487e7b768e6bb63aaae07a5b618a298f37a
AquafinaSun <aquafina.sun@outlook.com>
2025-06-29 20:56:41 +0800

    feat: support font selection in tinymce editor toolbar

--- src/main.ts
+++ src/main.ts
@@ -20,11 +20,13 @@ void tinymce.init({
   selector: "#tinymce-editor",
   plugins: "lists link code",
   toolbar:
-    "undo redo | forecolor | bold italic underline strikethrough overline subscript superscript | alignleft aligncenter alignright | bullist numlist | code | letterspacing letterwidth",
+    "undo redo | fontfamily | forecolor | bold italic underline strikethrough overline subscript superscript | alignleft aligncenter alignright | bullist numlist | code | letterspacing letterwidth",
   menubar: false,
   skin: false,
   content_css: false,
   license_key: "gpl",
+  font_family_formats:
+    'Arial=arial; Tahoma=tahoma; Verdana=verdana; 楷体=simkai, SimKai, KaiTi, serif; 黑体=simhei, SimHei, "PingFang SC"; 宋体=simsun, SimSun, serif;',
   formats: {
     letterspacing: {
       inline: "span",
@@ -51,24 +53,24 @@ void tinymce.init({
     // Register custom overline icon
     editor.ui.registry.addIcon(
       "overline",
-      '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M5 4h14v1.5H5V4zm7 3.5c3.04 0 5.5 2.46 5.5 5.5v4.5h-2.25v-4.5c0-1.79-1.46-3.25-3.25-3.25S8.75 11.21 8.75 13v4.5H6.5V13c0-3.04 2.46-5.5 5.5-5.5z"/></svg>',
+      `<svg width='24' height='24' viewBox='0 0 24 24'><path d='M5 4h14v1.5H5V4zm7 3.5c3.04 0 5.5 2.46 5.5 5.5v4.5h-2.25v-4.5c0-1.79-1.46-3.25-3.25-3.25S8.75 11.21 8.75 13v4.5H6.5V13c0-3.04 2.46-5.5 5.5-5.5z'/></svg>`,
     );
 
     // Register custom letterspacing icon
     editor.ui.registry.addIcon(
       "letterspacing",
-      '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M4 6h2v12H4V6zm3 0h1v12H7V6zm2 0h1v12H9V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h2v12h-2V6z"/></svg>',
+      `<svg width='24' height='24' viewBox='0 0 24 24'><path d='M4 6h2v12H4V6zm3 0h1v12H7V6zm2 0h1v12H9V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h1v12h-1V6zm2 0h2v12h-2V6z'/></svg>`,
     );
 
     // Register custom letterwidth icon
     editor.ui.registry.addIcon(
       "letterwidth",
-      `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8 20 L12 4 L16 20 M10 15 H14" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-        <path d="M2 12h5" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-        <path d="M4 10l-2 2 2 2" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M22 12h-5" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-        <path d="M20 10l2 2-2 2" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+      `<svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M8 20 L12 4 L16 20 M10 15 H14' stroke='currentColor' stroke-width='1.8' stroke-linecap='round' stroke-linejoin='round' fill='none'/>
+        <path d='M2 12h5' stroke='currentColor' stroke-width='1.2' stroke-linecap='round'/>
+        <path d='M4 10l-2 2 2 2' stroke='currentColor' stroke-width='1.2' stroke-linecap='round' stroke-linejoin='round'/>
+        <path d='M22 12h-5' stroke='currentColor' stroke-width='1.2' stroke-linecap='round'/>
+        <path d='M20 10l2 2-2 2' stroke='currentColor' stroke-width='1.2' stroke-linecap='round' stroke-linejoin='round'/>
       </svg>`,
     );
 
@@ -109,7 +111,7 @@ void tinymce.init({
                 editor.execCommand(
                   "mceInsertContent",
                   false,
-                  `<span style="letter-spacing: ${letterSpacing}">${selectedContent}</span>`,
+                  `<span style='letter-spacing: ${letterSpacing}'>${selectedContent}</span>`,
                 );
               }
             }
@@ -162,7 +164,7 @@ void tinymce.init({
                 editor.execCommand(
                   "mceInsertContent",
                   false,
-                  `<span style="display: inline-block; transform: scaleX(${letterWidth}); transform-origin: left;">${selectedContent}</span>`,
+                  `<span style='display: inline-block; transform: scaleX(${letterWidth}); transform-origin: left;'>${selectedContent}</span>`,
                 );
               }
             }
